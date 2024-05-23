4a82d174c069d249b468a091a119efe9474618aa
AlbertDoggyLin <albertlin2468@gmail.com>
2024-05-23 14:45:00 +0800

    reformat files, switch to wsgi

--- server.py
+++ server.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template
 from gevent import pywsgi
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/scoreboard')
 def scoreboard():
     return render_template('scoreboard.html')
 
+
 if __name__=='__main__':
-    # import sys
-    # try:
-    #     port = int(sys.argv[1])
-    # except:
-    #     port = 5000
-    # server = pywsgi.WSGIServer(('0.0.0.0', port), app)
-    # server.serve_forever()
-    app.run(debug=True)
+    import sys
+    try:
+        port = int(sys.argv[1])
+    except:
+        port = 5000
+    server = pywsgi.WSGIServer(('0.0.0.0', port), app)
+    server.serve_forever()
+    # app.run(debug=True)
